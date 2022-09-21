f2279c13a1ff173512fd2e6f987e5eba9d5b24b5
Angel Marte <robotking134@gmail.com>
2022-09-21 13:08:41 -0400

    commit tree c3dff8c9754009afee54789a879b7442ca311d7b

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a,b){
+return a + b;
+}
+add(866,873)
+function subtract(a,b){
+return a - b;
+} 
+subtract (1,869)
+function multiply(a,b){
+return a * b
+}
+multiply (4,164590)
+function divide(a,b){
+    return a / b;
+} divide(2.577023498694517,1)
+
+function increment(a){
+    return a+=1
+}
+function decrement(a){
+    return a-=1
+}
+function makeInt(a){
+    return parseInt(a,10);
+}
+function preserveDecimal(a){
+    return parseFloat(a,10)
+
+}
+
+
